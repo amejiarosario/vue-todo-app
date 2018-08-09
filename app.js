19b13b278d7083276bc4dba97f08ffa0c84c88bb
Adrian Mejia <admejiar@cisco.com>
2018-08-09 12:57:55 -0400

    @keyup @blur @dblclick v-model.trim for editing todos

--- app.js
+++ app.js
@@ -5,12 +5,24 @@ const app = new Vue({
       { text: 'Learn JavaScript ES6+ goodies', isDone: true },
       { text: 'Learn Vue', isDone: false },
       { text: 'Build something awesome', isDone: false },
-    ]
+    ],
+    editingTodo: null
   },
   methods: {
     destroy (todo) {
       const index = this.todos.indexOf(todo);
       this.todos.splice(index, 1);
-    }
+    },
+    startEditing(todo) {
+      this.editingTodo = todo;
+      this.beforeText = todo.text;
+    },
+    finishEditing(todo) {
+      this.editingTodo = null;
+    },
+    cancelEditing(todo) {
+      this.editingTodo = null;
+      todo.text = this.beforeText;
+    },
   }
 })
